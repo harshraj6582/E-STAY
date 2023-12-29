f0850831181cfbfb3f5a3e746342587302f401b3
harshraj6582 <harshraj211430@gmail.com>
2023-12-29 11:15:10 +0530

    Navbar

---
+++ e-stay/app/layout.tsx
@@ -0,0 +1,32 @@
+import type { Metadata } from 'next'
+import { Inter  , Nunito } from 'next/font/google'
+import './globals.css'
+import Navbar from './components/Navbar/Navbar'
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'E-Stay',
+  description: 'Plan Your Vacation',
+}
+
+const font = Nunito ({
+  subsets : ["latin"],
+});
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={font.className}>
+
+        <Navbar/>
+        {children}
+        
+        
+        </body>
+    </html>
+  )
+}
